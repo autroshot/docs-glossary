ecd15ed67a62ea90b983d6db91e11a803001b4f6
autroshot <autroshot@gamil.com>
2022-09-18 03:29:42 +0900

    글꼴 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider
+      theme={extendTheme({
+        fonts: {
+          heading: `'Malgun Gothic', sans-serif`,
+          body: `'Malgun Gothic', sans-serif`,
+        },
+      })}
+    >
       <Component {...pageProps} />
     </ChakraProvider>
   );
