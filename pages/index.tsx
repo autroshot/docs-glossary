a6b477039503e51dd8397a9eadb95f71637c7f67
autroshot <autroshot@gamil.com>
2022-09-18 14:50:32 +0900

    구현: CSV 파일 읽기

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,9 +9,12 @@ import {
   Link,
   Spacer,
 } from '@chakra-ui/react';
-import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import { InferGetStaticPropsType } from 'next';
 import Head from 'next/head';
 import GlossaryTable, { Term } from '../components/glossaryTable';
+import { readFile } from 'fs/promises';
+import Papa from 'papaparse';
+import path from 'path';
 
 export default function Home({
   terms,
@@ -57,30 +60,29 @@ export default function Home({
 }
 
 export async function getStaticProps() {
-  // TODO: CSV 파일에서 데이터 읽기
-  const terms: Term[] = [
-    {
-      english: 'application1',
-      korean: '앱, 애플리케이션',
-      type: '일반',
-      field: '',
-      comment: '',
+  const terms: Term[] = [];
+
+  const file = await readFile(
+    path.join(process.cwd(), 'public', '/glossary.csv'),
+    'utf-8'
+  );
+
+  Papa.parse<string[]>(file, {
+    complete(results) {
+      if (results.data.length !== 0) {
+        results.data.map((term, index) => {
+          if (index >= results.data.length - 1) return;
+          terms.push({
+            english: term[0] ?? '',
+            korean: term[1] ?? '',
+            comment: term[2] ?? '',
+            type: (term[3] ?? '') as '일반' | '고유',
+            field: term[4] ?? '',
+          });
+        });
+      }
     },
-    {
-      english: 'application2',
-      korean: '앱, 애플리케이션',
-      type: '일반',
-      field: '',
-      comment: '',
-    },
-    {
-      english: 'application3',
-      korean: '앱, 애플리케이션',
-      type: '일반',
-      field: '',
-      comment: '',
-    },
-  ];
+  });
 
   return { props: { terms } };
 }
