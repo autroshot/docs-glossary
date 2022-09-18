9a820824ec87cf91efa250d9485d2ebfada7f997
autroshot <autroshot@gamil.com>
2022-09-19 08:36:15 +0900

    리팩터링: 홈페이지의 favicon _document로 이동

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
     <Html lang="ko">
       <Head>
         <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
         <ColorModeScript />
