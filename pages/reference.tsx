3b04a057a7a66f4b0b1d2ab1dc652418d53ec311
autroshot <autroshot@gamil.com>
2022-09-19 08:35:20 +0900

    구현: 출처 페이지 생성

---
+++ pages/reference.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Reference() {
+  return (
+    <>
+      <Head>
+        <title>출처</title>
+      </Head>
+      <div>출처</div>
+    </>
+  );
+}
