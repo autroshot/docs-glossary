a8c8dc5f2138435586d010c0027444c2fed69047
autroshot <autroshot@gamil.com>
2022-09-19 06:40:22 +0900

    구현: 네비바의 다크 모드 버튼 클릭 시 다크 모드 토글

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,19 @@
 import { MoonIcon } from '@chakra-ui/icons';
-import { Box, Flex, IconButton, Spacer } from '@chakra-ui/react';
+import { Box, Flex, IconButton, Spacer, useColorMode } from '@chakra-ui/react';
 
 export default function Navbar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <Flex px={6} py={4} gap="5" align="center">
       <Box>용어집</Box>
       <Box>출처</Box>
       <Spacer />
-      <IconButton aria-label="다크 모드" icon={<MoonIcon />} />
+      <IconButton
+        aria-label="다크 모드"
+        icon={<MoonIcon />}
+        onClick={toggleColorMode}
+      />
     </Flex>
   );
 }
