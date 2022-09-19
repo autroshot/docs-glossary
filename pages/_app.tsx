827d380f6fe5aa810f7ba73780793df55ee470b5
autroshot <autroshot@gamil.com>
2022-09-19 11:37:01 +0900

    구현: 구글 태그

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,14 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../themes';
 import Layout from '../components/layout';
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'G-XQEH2MX3X1' });
+  }, []);
+
   return (
     <ChakraProvider theme={theme}>
       <Layout>
