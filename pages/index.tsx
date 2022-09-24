268a74342ee4d31a8443ac03ae7ecbbd8a5ca9fb
autroshot <autroshot@gamil.com>
2022-09-24 12:09:52 +0900

    구현: 용어집 검색 기능

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,36 @@
-import { Container, Heading } from '@chakra-ui/react';
+import {
+  Container,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from '@chakra-ui/react';
 import { InferGetStaticPropsType } from 'next';
 import Head from 'next/head';
 import GlossaryTable, { Term } from '../components/glossaryTable';
 import { readFile } from 'fs/promises';
 import Papa from 'papaparse';
 import path from 'path';
+import { SearchIcon } from '@chakra-ui/icons';
+import { useState } from 'react';
 
 export default function Home({
   terms,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const [searchWord, setSearchWord] = useState('');
+
+  let filteredTerms: Term[] = [];
+  if (searchWord.length === 0) {
+    filteredTerms = [...terms];
+  } else {
+    filteredTerms = terms.filter((term) => {
+      return (
+        term.english.toLowerCase().includes(searchWord.toLowerCase()) ||
+        term.korean.includes(searchWord)
+      );
+    });
+  }
+
   return (
     <>
       <Head>
@@ -16,7 +38,17 @@ export default function Home({
       </Head>
       <Container centerContent maxW="container.xl" my={5}>
         <Heading my={3}>용어집</Heading>
-        <GlossaryTable terms={terms} />
+        <InputGroup my={3}>
+          <Input
+            placeholder="검색할 영어나 한국어를 입력하세요."
+            value={searchWord}
+            onChange={(event) => setSearchWord(event.currentTarget.value)}
+          />
+          <InputRightElement>
+            <SearchIcon />
+          </InputRightElement>
+        </InputGroup>
+        <GlossaryTable terms={filteredTerms} />
       </Container>
     </>
   );
