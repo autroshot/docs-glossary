85b04ff3db4c4836f2c322578ed8af08a17f4c5e
autroshot <autroshot@gamil.com>
2022-09-27 00:35:07 +0900

    구현: 용어집 테이블 CSS 수정

--- components/glossaryTable.tsx
+++ components/glossaryTable.tsx
@@ -7,18 +7,19 @@ import {
   Td,
   TableContainer,
 } from '@chakra-ui/react';
+import styles from './glossaryTable.module.css';
 
 export default function GlossaryTable({ terms }: Props) {
   return (
-    <TableContainer>
-      <Table variant="simple" my={3}>
+    <TableContainer whiteSpace="normal">
+      <Table variant="simple" my={3} className={styles.table} w="75rem">
         <Thead>
           <Tr>
-            <Th>영어</Th>
-            <Th>한국어</Th>
-            <Th>종류</Th>
-            <Th>분야</Th>
-            <Th>설명</Th>
+            <Th w="20%">영어</Th>
+            <Th w="24%">한국어</Th>
+            <Th w="8%">종류</Th>
+            <Th w="13%">분야</Th>
+            <Th w="35%">설명</Th>
           </Tr>
         </Thead>
         <Tbody>
