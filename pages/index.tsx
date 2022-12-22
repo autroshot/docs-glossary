27808eccf7344a526c5d740a5e14c0a0b6c04606
autroshot <autroshot@gmail.com>
2022-12-23 04:55:23 +0900

    설정/구현: tsconfig의 target을 ES6로 변경. MyStorage 클래스 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -160,4 +160,44 @@ export default function Home() {
       );
     });
   }
+
+  class MyStorage {
+    readonly #storage: Storage;
+
+    constructor(storage: Storage) {
+      this.#storage = storage;
+    }
+
+    getTerms() {
+      const value = this.#get('terms');
+      if (value === null) return null;
+
+      const parsedValue = JSON.parse(value);
+      if (!this.#isTermArray(parsedValue)) return null;
+      return parsedValue;
+    }
+    setTerms(terms: Term[]) {
+      this.#set('terms', JSON.stringify(terms));
+    }
+
+    #get(key: myStorageKeys): null | string {
+      return this.#storage.getItem(key);
+    }
+    #set(key: myStorageKeys, value: string): void {
+      return this.#storage.setItem(key, value);
+    }
+
+    #isTermArray(object: any): object is Term[] {
+      return (
+        object &&
+        Array.isArray(object) &&
+        typeof object[0].english === 'string' &&
+        typeof object[0].korean === 'string' &&
+        typeof object[0].type === 'string' &&
+        typeof object[0].field === 'string' &&
+        typeof object[0].description === 'string'
+      );
+    }
+  }
+  type myStorageKeys = 'terms' | 'updatedTimestamp';
 }
