3cf9fbdaf99e1762255ea596b934c6b245390389
autroshot <autroshot@gmail.com>
2022-12-23 14:28:57 +0900

    테스트: millisecondsToBriefText 함수에 대한 테스트 일부 작성

---
+++ components/update.test.ts
@@ -0,0 +1,8 @@
+import { expect, test } from '@jest/globals';
+import { millisecondsToBriefText } from './update';
+
+test('millisecondsToBriefText 함수', () => {
+  expect(millisecondsToBriefText(5000)).toBe('조금');
+  expect(millisecondsToBriefText(0)).toBe('조금');
+  expect(millisecondsToBriefText(-5000)).toBe('조금');
+});
