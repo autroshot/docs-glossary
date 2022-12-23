1ee765759aae115b0219bd7670c9295925666955
autroshot <autroshot@gmail.com>
2022-12-23 23:15:36 +0900

    테스트: millisecondsToBriefText 함수에 대한 테스트 작성

--- components/update.test.ts
+++ components/update.test.ts
@@ -1,7 +1,32 @@
 import { millisecondsToBriefText } from './update';
 
 test('millisecondsToBriefText 함수', () => {
-  expect(millisecondsToBriefText(5000)).toBe('조금');
+  expect(millisecondsToBriefText(-5 * 1000)).toBe('조금');
   expect(millisecondsToBriefText(0)).toBe('조금');
-  expect(millisecondsToBriefText(-5000)).toBe('조금');
+  expect(millisecondsToBriefText(5 * 1000)).toBe('조금');
+  expect(millisecondsToBriefText(60 * 1000 - 1)).toBe('조금');
+
+  expect(millisecondsToBriefText(1 * 60 * 1000)).toBe('1분');
+  expect(millisecondsToBriefText(1766123)).toBe('29분');
+  expect(millisecondsToBriefText(60 * 60 * 1000 - 1)).toBe('59분');
+
+  expect(millisecondsToBriefText(1 * 60 * 60 * 1000)).toBe('1시간');
+  expect(millisecondsToBriefText(75123456)).toBe('20시간');
+  expect(millisecondsToBriefText(24 * 60 * 60 * 1000 - 1)).toBe('23시간');
+
+  expect(millisecondsToBriefText(24 * 60 * 60 * 1000)).toBe('1일');
+  expect(millisecondsToBriefText(425600000)).toBe('4일');
+  expect(millisecondsToBriefText(7 * 24 * 60 * 60 * 1000 - 1)).toBe('6일');
+
+  expect(millisecondsToBriefText(7 * 24 * 60 * 60 * 1000)).toBe('1주');
+  expect(millisecondsToBriefText(1809600000)).toBe('2주');
+  expect(millisecondsToBriefText(30 * 24 * 60 * 60 * 1000)).toBe('4주');
+
+  expect(millisecondsToBriefText(31 * 24 * 60 * 60 * 1000)).toBe('1개월');
+  expect(millisecondsToBriefText(13150080000)).toBe('5개월');
+  expect(millisecondsToBriefText(365 * 24 * 60 * 60 * 1000)).toBe('11개월');
+
+  expect(millisecondsToBriefText(365.5 * 24 * 60 * 60 * 1000)).toBe('1년');
+  expect(millisecondsToBriefText(157896000000)).toBe('5년');
+  expect(millisecondsToBriefText(15789600000000)).toBe('500년');
 });
--- components/update.tsx
+++ components/update.tsx
@@ -14,27 +14,27 @@ export default function Update({ updatedTimestamp }: Props) {
 }
 
 export function millisecondsToBriefText(milliseconds: number): string {
-  const SECONDS_IN_A_YEAR = 31556926;
-  const SECONDS_IN_A_MONTH = 2629743;
-  const SECONDS_IN_A_WEEK = 604800;
-  const SECONDS_IN_A_DAY = 86400;
-  const SECONDS_IN_AN_HOUR = 3600;
+  const SECONDS_IN_A_YEAR = 31556926; // 대략 365.24 * 24 * 60 * 60
+  const SECONDS_IN_A_MONTH = 2629743; // 대략 30.44 * 24 * 60 * 60
+  const SECONDS_IN_A_WEEK = 604800; // 7 * 24 * 60 * 60
+  const SECONDS_IN_A_DAY = 86400; // 24 * 60 * 60
+  const SECONDS_IN_AN_HOUR = 3600; // 60 * 60
   const SECONDS_IN_A_MINUTE = 60;
 
   const seconds = milliseconds / 1000;
 
   if (seconds >= SECONDS_IN_A_YEAR)
-    return `${Math.floor(seconds / SECONDS_IN_A_YEAR)} 년`;
+    return `${Math.floor(seconds / SECONDS_IN_A_YEAR)}년`;
   if (seconds >= SECONDS_IN_A_MONTH)
-    return `${Math.floor(seconds / SECONDS_IN_A_MONTH)} 개월`;
+    return `${Math.floor(seconds / SECONDS_IN_A_MONTH)}개월`;
   if (seconds >= SECONDS_IN_A_WEEK)
-    return `${Math.floor(seconds / SECONDS_IN_A_WEEK)} 주`;
+    return `${Math.floor(seconds / SECONDS_IN_A_WEEK)}주`;
   if (seconds >= SECONDS_IN_A_DAY)
-    return `${Math.floor(seconds / SECONDS_IN_A_DAY)} 일`;
+    return `${Math.floor(seconds / SECONDS_IN_A_DAY)}일`;
   if (seconds >= SECONDS_IN_AN_HOUR)
-    return `${Math.floor(seconds / SECONDS_IN_AN_HOUR)} 시간`;
+    return `${Math.floor(seconds / SECONDS_IN_AN_HOUR)}시간`;
   if (seconds >= SECONDS_IN_A_MINUTE)
-    return `${Math.floor(seconds / SECONDS_IN_A_MINUTE)} 분`;
+    return `${Math.floor(seconds / SECONDS_IN_A_MINUTE)}분`;
   return '조금';
 }
 
