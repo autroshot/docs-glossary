51e6a8e9bf2ae6be50d47c519fd493c3fecb401a
autroshot <autroshot@gmail.com>
2023-02-08 02:48:11 +0900

    구현: 세부 서랍 외관

--- components/detailDrawer.tsx
+++ components/detailDrawer.tsx
@@ -1,12 +1,16 @@
 import {
-  Button,
+  Box,
+  Container,
   Drawer,
   DrawerBody,
   DrawerCloseButton,
   DrawerContent,
-  DrawerFooter,
   DrawerHeader,
   DrawerOverlay,
+  Heading,
+  StackDivider,
+  Text,
+  VStack,
 } from '@chakra-ui/react';
 import { RefObject } from 'react';
 
@@ -17,21 +21,50 @@ export default function DetailDrawer({
 }: Props) {
   return (
     <Drawer
-      placement="bottom"
+      size="lg"
       isOpen={isOpen}
       onClose={onClose}
       finalFocusRef={finalFocusRef}
     >
       <DrawerOverlay />
       <DrawerContent>
-        <DrawerCloseButton />
-        <DrawerHeader>용어 상세</DrawerHeader>
-
-        <DrawerBody>내용</DrawerBody>
-
-        <DrawerFooter>
-          <Button onClick={onClose}>닫기</Button>
-        </DrawerFooter>
+        <Container maxW="container.md" maxH="100%" overflowY="auto">
+          <DrawerHeader fontSize="2xl">accessibility</DrawerHeader>
+          <DrawerCloseButton />
+          <DrawerBody>
+            <VStack
+              spacing={3}
+              align="stretch"
+              divider={<StackDivider borderColor="gray.200" />}
+            >
+              <Box>
+                <Heading size="xs">한국어</Heading>
+                <Text mt="2">프로퍼티</Text>
+              </Box>
+              <Box>
+                <Heading size="xs">유형</Heading>
+                <Text mt="2">일반</Text>
+              </Box>
+              <Box>
+                <Heading size="xs">분야</Heading>
+                <Text mt="2">리액트</Text>
+              </Box>
+              <Box>
+                <Heading size="xs">설명</Heading>
+                <Text mt="2">
+                  원래 뜻은 속성이지만 attribute와의 구분을 위해 프로퍼티를 사용
+                  원래 뜻은 속성이지만 attribute와의 구분을 위해 프로퍼티를 사용
+                </Text>
+              </Box>
+              <Box>
+                <Heading size="xs">출처</Heading>
+                <Text mt="2">
+                  http://terms.tta.or.kr/dictionary/dictionaryView.do?word_seq=053425-2http://terms.tta.or.kr/dictionary/dictionaryView.do?word_seq=053425-2http://terms.tta.or.kr/dictionary/dictionaryView.do?word_seq=053425-2http://terms.tta.or.kr/dictionary/dictionaryView.do?word_seq=053425-2http://terms.tta.or.kr/dictionary/dictionaryView.do?word_seq=053425-2http://terms.tta.or.kr/dictionary/dictionaryView.do?word_seq=053425-2http://terms.tta.or.kr/dictionary/dictionaryView.do?word_seq=053425-2
+                </Text>
+              </Box>
+            </VStack>
+          </DrawerBody>
+        </Container>
       </DrawerContent>
     </Drawer>
   );
