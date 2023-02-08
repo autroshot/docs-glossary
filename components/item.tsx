092c41c1c0a6b0fc22c96427c473dccb9d69e94f
autroshot <autroshot@gmail.com>
2023-02-08 11:35:20 +0900

    구현: Item 컴포넌트 - 제목의 CSS 수정

--- components/item.tsx
+++ components/item.tsx
@@ -1,9 +1,16 @@
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { Box, Heading, Text, useColorMode } from '@chakra-ui/react';
 
 export default function Item({ title, content }: Props) {
+  const { colorMode } = useColorMode();
+
   return (
     <Box>
-      <Heading size="xs">{title}</Heading>
+      <Heading
+        fontSize="xs"
+        color={colorMode === 'light' ? 'gray.600' : 'gray.400'}
+      >
+        {title}
+      </Heading>
       <Text mt="2">{content}</Text>
     </Box>
   );
